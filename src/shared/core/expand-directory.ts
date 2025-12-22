dba725ccdfb9b13d8ab62366a03a360898d437e1
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-12-22 18:35:45 +0100

    fix: expanding directory only works on directories that have not been open

--- src/shared/core/expand-directory.ts
+++ src/shared/core/expand-directory.ts
@@ -3,10 +3,11 @@ import { addToFileTree } from "../utils/add-to-file-tree";
 import { getFileTree } from "./get-file-tree";
 
 export async function expandDirectory(dirPath: string): Promise<void> {
-  const childNode = await getFileTree(dirPath);
   const node = fileTree.get(dirPath);
 
-  if (node && node.type === "directory") {
+  if (node && node.type === "directory" && !node.expanded) {
+    const childNode = await getFileTree(dirPath);
+
     node.expanded = true;
     node.children = childNode;
 
