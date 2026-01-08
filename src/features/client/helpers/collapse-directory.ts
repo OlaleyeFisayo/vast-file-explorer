7114570284235b5e597c79b2d9d38cda1a0c2027
OlaleyeFisayo <festusolaleyef@gmail.com>
2026-01-08 14:45:09 +0100

    chore: file structure changed

--- src/features/client/helpers/collapse-directory.ts
+++ src/features/client/helpers/collapse-directory.ts
@@ -1,6 +1,6 @@
-import type { FileTreeNode } from "../../features/file-explorer/types";
+import type { FileTreeNode } from "../../file-explorer/types";
 
-import { clientInstance } from "../../shared/variables";
+import { clientInstance } from "../../../shared/variables";
 
 export async function collapseDirectory(path: string): Promise<FileTreeNode[]> {
   const response = await clientInstance.post<FileTreeNode[]>("/collapse", { path });
--- src/features/client/helpers/create-file.ts
+++ src/features/client/helpers/create-file.ts
@@ -1,4 +1,4 @@
-import { clientInstance } from "../../shared/variables";
+import { clientInstance } from "../../../shared/variables";
 
 export async function createFile(name: string, dirPath?: string): Promise<void> {
   await clientInstance.post("/", {
