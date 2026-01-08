98b4f24171453c6e28dfba84b37a22e8b58a7cd7
OlaleyeFisayo <festusolaleyef@gmail.com>
2026-01-08 15:28:37 +0100

    refact: changed the expand directory on file add, that should be done on the front end

--- src/features/directory-watcher/helpers/on-file-and-folder-add.ts
+++ src/features/directory-watcher/helpers/on-file-and-folder-add.ts
@@ -7,7 +7,6 @@ import path from "node:path";
 import type { FileTreeNode } from "../../file-explorer/types";
 
 import { globalOptions } from "../../../shared/variables";
-import { expandDirectory } from "../../file-explorer/helpers/expand-directory";
 import { sortFileTreeNodes } from "../../file-explorer/helpers/sort-file-tree-nodes";
 import {
   FileTree,
@@ -66,20 +65,15 @@ export async function onFileAndFolderAdd(filePath: string): Promise<void> {
 
   const resolvedParentDir = await realpath(parentDir);
   const parentNode = SearchIndex.get(resolvedParentDir);
-  if (parentNode && parentNode.type === "directory") {
-    if (!parentNode.expanded) {
-      await expandDirectory(resolvedParentDir);
+  if (parentNode && parentNode.type === "directory" && parentNode.expanded) {
+    const existingIndex = parentNode.children.findIndex(node => node.path === realFilePath);
+    if (existingIndex !== -1) {
+      parentNode.children[existingIndex] = newNode;
     }
     else {
-      const existingIndex = parentNode.children.findIndex(node => node.path === realFilePath);
-      if (existingIndex !== -1) {
-        parentNode.children[existingIndex] = newNode;
-      }
-      else {
-        parentNode.children.push(newNode);
-      }
-
-      parentNode.children = sortFileTreeNodes(parentNode.children);
+      parentNode.children.push(newNode);
     }
+
+    parentNode.children = sortFileTreeNodes(parentNode.children);
   }
 }
